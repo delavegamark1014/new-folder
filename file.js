2080be6493ba6b754ea81876c71f87448406ce42
delavegamark1014 <delavegamark1014@gmail.com>
2023-02-16 11:57:43 -0800

    init

---
+++ file.js
@@ -0,0 +1,37 @@
+alert("Help!");
+console.log("Help me!");
+let myScore = 0
+let cScore= 0
+    function cTurn() {
+        const randNum=Math.floor(Math.random()*3+1)
+        switch(randNum) {
+            case 1:
+                computer='rock'
+                
+                break;
+            case 2:
+                computer = 'paper'
+                
+                break;
+            case 3:
+                computer = 'scissors'
+                
+                break;
+            }
+        }
+    function checkWinner() {
+        let player = prompt('pick')
+        
+        if (player==computer) {
+            return `draw, the score is ${myscore}:${cScore}`
+        } else if (computer=='rock'){
+            return (player=='paper') ? `you win(paper beat rock), the score is ${myScore+=1}:${cScore}` : `you lose(cpu picked rock) the score is ${myScore}:${cScore+=1}`
+        }  else if (computer=='paper'){
+            return (player=='scissors') ? `you win(scissors beat paper), the score is ${myScore+=1}:${cScore}` : `you lose(cpu picked paper)the score is ${myScore}:${cScore+=1}`
+        } else if (computer=='scissors'){
+            return (player=='rock') ? `you win(rock beats scissors), the score is ${myScore+=1}:${cScore}` : `you lose(cpu picked scissors) the score is ${myScore}:${cScore+=1}`
+        }
+    }
+
+       
+            
---
+++ rps.js
@@ -0,0 +1,21 @@
+const options = ['rock','paper','scissors']
+function getComputerChoice() {
+    const choice = options[Math.floor(Math.random()*options.length)]
+    return choice
+}
+function checkWinner(playerSelection,computerSelection) {
+    if (playerSelection===computerSelection) {
+        return 'tie'
+    } 
+    else if(
+        (playerSelection==='rock' && computerSelection==='scissors') ||
+        (playerSelection==='paper' && computerSelection==='rock') ||
+        (playerSelection==='scissors' && computerSelection==='paper')
+    ){
+        return 'player'
+    } else {
+        return 'computer'
+    }
+}
+
+
